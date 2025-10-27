0564a7dd0f0d871bdb13d2e19570d2f69eb56bc3
s1140362-rgb <s1140362@dmhs.kh.edu.tw>
2025-10-27 16:13:31 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int age = 16;
+    double height = 157.8;
+    std::string name = "Sammi";
+    bool is_Student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << height << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_Student << std::endl;
+    
+    
+    return 0;
+}
