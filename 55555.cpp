d3bb58f95c685d27cb5a6e16ec17a3a2dfb9d284
s1140362-rgb <s1140362@dmhs.kh.edu.tw>
2025-11-07 11:36:19 +0800

    hw3

---
+++ 55555.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"開\t 心\t 開\t 心"<< std::endl;
+    std::cout<<"開\n 心\n 開\n 心"<< std::endl;
+    std::cout<<"開 心 開 心"<< std::endl;
+    std::cout<<"開 心 開 心"<< std::endl;
+    std::cout<<"\"開心開心\""<< std::endl;
+    std::cout<<"開 心 開 心"<< std::endl;
+    std::cout<<"\\開 心 開 心\\"<< std::endl;
+
+    return 0;
+}
