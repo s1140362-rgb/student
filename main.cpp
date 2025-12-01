7d87efa3d06609b2f056472992e419b18252e7fa
s1140362-rgb <s1140362@dmhs.kh.edu.tw>
2025-12-01 16:00:22 +0800

    hw 9

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    double height, weight, bmi;
-
-    cout << "請輸入身高(cm): ";
-    cin >> height;
-
-    cout << "請輸入體重(kg): ";
-    cin >> weight;
-
-    // 將身高轉換為公尺
-    height = height / 100.0;
-
-    // 計算 BMI
-    bmi = weight / (height * height);
-
-    cout << "您的 BMI = " << bmi << endl;
-    if (18.5 < bmi) {
-        cout << "體重過輕" << endl;
-    }else if (24.9 > bmi) {
-        cout << "體重正常" << endl;
-    }else {
-        cout << "體重過重" << endl;
+int main()
+{   
+    int sum = 0;
+    for (int i=1;i<6;i++) {
+        std::cout << "現在i值:" << i << std::endl;
+        sum = sum + 1;
+        std::cout <<"回圈內 sum 值:" << sum << std::endl;
     }
-   return 0;
+    std::cout << "1+..+5 總合為:" << sum;
+
+    return 0;
 }
